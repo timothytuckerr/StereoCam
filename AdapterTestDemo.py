f4e4abcebb86a3b9b6fda527ce437f66e8084e4d
tim tucker <timothytucker@berkeley.edu>
2021-02-09 18:07:05 -0800

    test change

--- AdapterTestDemo.py
+++ AdapterTestDemo.py
@@ -21,28 +21,24 @@ gp.output(21, True)
 gp.output(22, True)
 
 def main():
-    print"Start testing the camera A"
     i2c = "i2cset -y 1 0x70 0x00 0x04"
     os.system(i2c)
     gp.output(7, False)
     gp.output(11, False)
     gp.output(12, True)
     capture(1)
-    print"Start testing the camera B"   
     i2c = "i2cset -y 1 0x70 0x00 0x05"
     os.system(i2c)
     gp.output(7, True)
     gp.output(11, False)
     gp.output(12, True)
     capture(2)
-    print"Start testing the camera C"
     i2c = "i2cset -y 1 0x70 0x00 0x06"
     os.system(i2c)
     gp.output(7, False)
     gp.output(11, True)
     gp.output(12, False)
     capture(3)
-    print"Start testing the camera D"
     i2c = "i2cset -y 1 0x70 0x00 0x07"
     os.system(i2c)
     gp.output(7, True)
