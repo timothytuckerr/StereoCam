4437f9f625d12663acf7ace0e15ce60ed4b1f185
tim tucker <timothytucker@berkeley.edu>
2021-02-09 18:31:44 -0800

    timing test for demo

--- AdapterTestDemo.py
+++ AdapterTestDemo.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as gp
 import os
+import time
 
 gp.setwarnings(False)
 gp.setmode(gp.BOARD)
@@ -21,12 +22,28 @@ gp.output(21, True)
 gp.output(22, True)
 
 def main():
+    times = []
+    time1 = time.perf_counter()
     i2c = "i2cset -y 1 0x70 0x00 0x04"
     os.system(i2c)
+    time2 = time.perf_counter()
+    times.append(time2 - time1)
+    time1 = time.perf_counter()
     gp.output(7, False)
+    time2 = time.perf_counter()
+    times.append(time2 - time1)
+    time1 = time.perf_counter()
     gp.output(11, False)
+    time2 = time.perf_counter()
+    times.append(time2 - time1)
+    time1 = time.perf_counter()
     gp.output(12, True)
+    time2 = time.perf_counter()
+    times.append(time2 - time1)
+    time1 = time.perf_counter()
     capture(1)
+    time2 = time.perf_counter()
+    times.append(time2 - time1)
     i2c = "i2cset -y 1 0x70 0x00 0x05"
     os.system(i2c)
     gp.output(7, True)
@@ -45,6 +62,11 @@ def main():
     gp.output(11, True)
     gp.output(12, False)
     capture(4)
+    print("os: " + times[0])
+    print("gp7: " + times[1])
+    print("gp11: " + times[2])
+    print("gp12: " + times[3])
+    print("cap: " + times[4])
 
 def capture(cam):
     cmd = "raspistill -o capture_%d.jpg" % cam
